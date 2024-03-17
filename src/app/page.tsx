844df13f498100d6c14fe50a86ca1b2fcf28956a
Minh Hung <nlmhung2511@gmail.com>
2024-03-17 09:01:09 +0700

    add page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PiBookOpenFill } from "react-icons/pi";
 
 export default function Home() {
   return (
-    <div className='p-3 pt-4 text-center '>
+    <div className='p-3 pt-4 text-center'>
       <PiBookOpenFill className="inline"/>
       <h1 className='uppercase font-bold'>New Comics</h1>
       <Container/>
