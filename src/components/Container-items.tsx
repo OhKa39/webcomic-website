00dc8d91ed48bfd8e035412e4cdbcb9aa0fad38e
Minh Hung <nlmhung2511@gmail.com>
2024-03-17 19:27:02 +0700

    add slug pages, more UI effect

--- src/components/Container-items.tsx
+++ src/components/Container-items.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+
+
 export default function Containeritems({data}) {
   return (   
-    data.map((result) => (
-        <div key={result.id} className='rounded border-amber-400 bg-slate-200 dark:bg-amber-400 border-4 '>          
+    data.map((result) => (      
+        <Link href={`/comic/${result.name}`} className='rounded border-amber-400 bg-slate-200 dark:bg-amber-400 border-4 '>          
             <Image 
-                src={result.linkImg}
-                width={100}
-                height={100}
+                src={result.imageLink}
+                width={150}
+                height={150}
                 style={{
                     width: '100%',
-                    height: '210px',
+                    height: '100%',
                   }}
                   alt="Picture of comic"
+                  className='hover:opacity-80 transition-opacity duration-300'
             />
-            <p>{result.link}</p>
             <p className='font-bold truncate'>{result.name}</p>
-        </div>
+        </Link>
     ))  
   )
 }
