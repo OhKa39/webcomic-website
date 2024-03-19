498f72b2a9bd784f8921785fca791ca58736b41c
Minh Hung <nlmhung2511@gmail.com>
2024-03-19 19:17:33 +0700

    fetch container and add comic page

--- src/components/ComicMenu.tsx
+++ src/components/ComicMenu.tsx
@@ -6,7 +6,7 @@ export default async function ComicMenu({data, path}){
     console.log(data)
     return (
         data.map((chapter) => (      
-            <div className=' overflow-auto h-48'>
+            <div className=' overflow-auto'>
                 <ol className='leading-8  hover:text-red-300 transition-opacity duration-300'>
                     <li><Link href= {`${path}/${chapter.chapterNumber}`}>Chương {chapter.chapterNumber}</Link></li>
                 </ol>
