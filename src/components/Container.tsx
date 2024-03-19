2a48df71266cc6c7f6666cbe4720d255738b3c98
Minh Hung <nlmhung2511@gmail.com>
2024-03-19 13:10:57 +0700

    fetch tu db len container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ContainerItems from './Container-items'
+import { PrismaClient } from '@prisma/client'
 //import axios from "axios";
 
 import file1 from 'D:/TruongDH/KTPM/1.json'
@@ -13,8 +14,22 @@ data.push(file3)
 
 
 
-export default function Container() {
-    // const data = [
+const prisma = new PrismaClient()
+
+async function getContainerItems() {
+    const allComics = await prisma.comics.findMany({
+        select: {
+            comicName: true,
+            comicImageLink: true,
+        }
+    })
+    //console.log(allComics)
+    return allComics
+}
+
+
+export default async function Container() {
+    // const data2 = [
     //     { id: "1", link: "1", linkImg: "/anh1.jpg", name: "truyen1", time: "10" },
     //     { id: "2", link: "2", linkImg: "/anh2.jpg", name: "truyen2", time: "10" },
     //     { id: "3", link: "3", linkImg: "/anh3.jpg", name: "truyen3", time: "10" },
@@ -22,7 +37,10 @@ export default function Container() {
     //     { id: "5", link: "5", linkImg: "/anh5.jpg", name: "truyen5", time: "10" },
     //     { id: "6", link: "6", linkImg: "/anh6.jpg", name: "truyen6", time: "10" }
     // ]
+    
+    const data = await getContainerItems()
 
+    //console.log(Array.isArray(data))
     return (
         <div className="grid grid-cols-5 mx-auto max-w-6xl py-4 w-full gap-5">
             <ContainerItems data={data} />
