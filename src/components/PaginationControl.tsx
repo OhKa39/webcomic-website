fa6f4922c8445280082c7a47be1cee48aa824654
nlminhhung <nlmhung2511@gmail.com>
2024-03-30 18:02:47 +0700

    fix deploy #2

--- src/components/PaginationControl.tsx
+++ src/components/PaginationControl.tsx
@@ -52,12 +52,15 @@ const PaginationControls: FC<PaginationControlsProps> = ({
               "bg-amber-400 shadow-lg from-default-500 to-default-800 text-white font-bold",
           }}
           onChange={(page: Number) => {
-            const paramObj = {
-              page: page.toString(),
-              categoryIds: categoryIds,
+            type typeParam = {
+              page: string;
+              categoryIds?: string;
             };
-            if (paramObj["categoryIds"] === null)
-              delete paramObj["categoryIds"];
+            const paramObj: typeParam = {
+              page: page.toString(),
+              categoryIds: categoryIds ?? "delete",
+            };
+            if (paramObj.categoryIds === "delete") delete paramObj.categoryIds;
             router.push(
               `${urlPage}${pathName}?${new URLSearchParams(paramObj)}`
             );
