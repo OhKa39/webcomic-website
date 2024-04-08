001041b28e8568a362c55c053f30d7d9ae65e3a8
Naalaya <anpt203@gmail.com>
2024-04-08 22:09:39 +0700

    update Follow Page

---
+++ src/app/api/following/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/db';
+
+export async function GET(req: NextRequest) {
+    try {
+        const pageNumber = Number(req.nextUrl.searchParams.get('page')) || 1;
+        const offset = Number(req.nextUrl.searchParams.get('offset'));
+
+        const countQuery = prisma.comics.count({
+            where: {
+                events: {
+                    some:{
+                        eventType:"FOLLOW"
+                    }
+                },
+            }
+        });
+
+        const comicsQuery = prisma.comics.findMany({
+            select: {
+                id: true,
+                comicName: true,
+                updatedAt: true,
+                comicImageLink: true,
+                // events:{
+                //     select:{
+                //     eventType:true
+                //     },
+                //     orderBy:{
+                //         createdAt:"desc"
+                //     },
+                //     where:{
+                //         eventType:"FOLLOW"
+                //     }
+                // }   
+            },
+            where: {
+                events: {
+                    some:{
+                        eventType:"FOLLOW"
+                    }
+                },
+            },
+            skip: (pageNumber - 1) * offset,
+            take: offset
+        });
+
+        const [totalComicsCount, comics] = await prisma.$transaction([
+            countQuery,
+            comicsQuery,
+          ]);
+      
+        const response = {
+            totalComicsCount,
+            comics,
+        };
+
+        console.log("comicsQuery",comicsQuery)
+
+
+        return NextResponse.json(response, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { message: `Something went wrong: ${error}` },
+            { status: 500 }
+        );
+    }
+}
--- src/app/follow/page.tsx
+++ src/app/follow/page.tsx
@@ -1,22 +1,46 @@
-"use client"
-import initialUser from "@/lib/initial-user"
+import React from "react";
+import { notFound } from "next/navigation";
+import Container from "@/components/Container";
+import PaginationControl from "@/components/PaginationControl";
+import { User, Events } from "@prisma/client"
 
 
-export default async function FollowPage() {
-    const user = await initialUser()
-    try {
-        if (!user) alert("Please Login First")
-    }
-    catch {
+const getComicData = async (page: any, offset: any, ctid: any) => {
+  const query = {
+    page: page,
+    offset: offset,
+  };
+  
+  const urlPage = process.env.NEXT_PUBLIC_URL;
+  let url = `${urlPage}/api/following?`;
+  Object.entries(query).forEach(([key, value]) => {
+    if (value !== undefined) url += key + "=" + value + "&";
+  });
+  const data = await fetch(url, { cache: "no-store" });
+  return data.json();
+};
 
-    }
-
-
-    return (
-        <div>
-            <h1>Truyện đã theo dõi</h1>
-
-        </div>
-    );
+export default async function SearchType({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    
+  const Page = searchParams["page"] ?? "1";
+  const categoryIDs = searchParams["categoryIds"];
+  let page = Number(Page);
+  if (page <= 0 || isNaN(page)) notFound();
+  const perPage = 40;
+  const comicFetch = getComicData(page, perPage, categoryIDs);
+  const [comicsData, ] = await Promise.all([
+    comicFetch,
+  ]);
+  const { totalComicsCount, comics } = comicsData;
+  return (
+    <div className="container mx-auto">
+        <h1 className = "mx-auto font-bold text-center text-2xl">Truyện đang theo dõi</h1>
+      <Container data={comics} />
+      <PaginationControl count={totalComicsCount} perPage={perPage} />
+    </div>
+  );
 }
-
