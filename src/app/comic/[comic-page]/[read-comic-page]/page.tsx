67825996ec7c3b50dacab11f8912c3f8776c4ee8
nlminhhung <nlmhung2511@gmail.com>
2024-04-13 10:39:27 +0700

    fix viewcount

--- src/app/comic/[comic-page]/[read-comic-page]/page.tsx
+++ src/app/comic/[comic-page]/[read-comic-page]/page.tsx
@@ -1,7 +1,6 @@
-"use client";
 import ComicPage from "@/app/comic/[comic-page]/[read-comic-page]/_components/ComicPage";
 import { Suspense } from "react";
-import visitedComics from "./_components/Visited-Comics"
+// import visitedComics from "./_components/Visited-Comics"
 import ChapterListBar from "./_components/ChapterListBar";
 
 const getPages = async (comicID: any, comicChapter: any) => {
@@ -17,15 +16,23 @@ const getData = async (comicID: any) => {
   return data.json();
 };
 
+const increaseViewCount = async (comicID: any, comicChapter: any) => {
+  const urlPage = process.env.NEXT_PUBLIC_URL;
+  const data = await fetch(`${urlPage}/api/comic/${comicID}/${comicChapter}`, {
+    method:'POST',
+    headers:{'Content-Type': 'application/json'}
+  });
+  return data.json();
+};
+
 
 export default async function ReadComicPage({ params }: { params: any }) {  
   const comicChapter = params["read-comic-page"]; // chapter cua thg comic do
   const comicId = params["comic-page"]; // id cua thg comic
-  visitedComics(comicChapter, comicId)
-  
   const pagesData = getPages(comicId, comicChapter); // lay trang trong chapter do
   const ListData = getData(comicId);
   const [pages, data] = await Promise.all([pagesData, ListData]);
+  increaseViewCount(comicId, comicChapter)
   const ListChapter = data.comicChapters; // lay mang gom cac chapter
   return (
     <div className="p-8 w-full bg-gray-400 relative justify-center">
