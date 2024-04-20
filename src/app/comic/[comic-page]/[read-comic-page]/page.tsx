040d4267c6e55f61af3e1705c17e205a83372d42
OhKa39 <95754226+OhKa39@users.noreply.github.com>
2024-04-20 12:04:35 +0700

    Update page.tsx

--- src/app/comic/[comic-page]/[read-comic-page]/page.tsx
+++ src/app/comic/[comic-page]/[read-comic-page]/page.tsx
@@ -2,13 +2,14 @@ import ComicPage from "@/app/comic/[comic-page]/[read-comic-page]/_components/Co
 import { Suspense } from "react";
 import ChapterListBar from "./_components/ChapterListBar";
 import initialUser from '@/lib/initial-user'
+import { FaRegCommentDots } from "react-icons/fa6";
+import CommentInput from "@/components/CommentInput";
+import CommentContainer from "@/components/CommentContainer";
 
 const getPages = async (comicID: any, comicChapter: any) => {
   const urlPage = process.env.NEXT_PUBLIC_URL;
 
-  const data = await fetch(`${urlPage}/api/comic/${comicID}/${comicChapter}`, {
-    cache: "no-cache",
-  }); // xoa
+  const data = await fetch(`${urlPage}/api/comic/${comicID}/${comicChapter}`); // xoa
   return data.json();
 };
 
@@ -44,16 +45,13 @@ export default async function ReadComicPage({
   const chapterId = pages["id"];
   
   userHistory(comicId, comicChapter, chapterId, profile)
-  if (profile) {
-    console.log("Login")
-  }
 
   const ListChapter = data.comicChapters; // lay mang gom cac chapter
   return (
     <div className="p-8 w-full bg-gray-400 relative justify-center">
-      {/* <Suspense> */}
-      <ComicPage data={pages["chapterImages"]} />
-      {/* </Suspense> */}
+      <Suspense>
+        <ComicPage data={pages["chapterImages"]} />
+      </Suspense>
 
       <div className="mt-2 bg-white dark:bg-gray-400 h-max px-12 sm:px-42 py-5 rounded-md">
         <div className="flex gap-3 items-center text-lg">
