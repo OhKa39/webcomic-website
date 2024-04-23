b858b75ffced852390050ce11a1f05bb824af42e
Minh Hung <nlmhung2511@gmail.com>
2024-04-23 20:25:15 +0700

    optimize signed in history page

--- src/app/history/_components/SignedIn-container.tsx
+++ src/app/history/_components/SignedIn-container.tsx
@@ -1,26 +1,32 @@
+'use client'
 import Container from "@/components/Container";
+import { useEffect, useState } from "react";
 
-const getData = async (profile: any) => {
-    const urlPage = process.env.NEXT_PUBLIC_URL;
-    const data = await fetch(`${urlPage}/api/SignedinHistory?userId=${profile.id}`);
-    return data.json();
-};
+export default function SignedInContainer({profile} : {profile: any}) {
+    const [data, setData] = useState([])
+    const [comicIdToDelete, setcomicIdToDelete] = useState("")
 
-export default async function SignedInContainer({profile} : {profile: any}) {
+    useEffect(()=> {
+        async function getData(profile: any) {
+            const urlPage = process.env.NEXT_PUBLIC_URL;
+            const comic = await fetch(`${urlPage}/api/SignedinHistory?userId=${profile.id}`)
+            .then((data) => data.json());
+            comic.forEach((i: any) => {
+                i.id = i.Comics.id;
+                i.comicName = i.Comics.comicName;
+                i.comicImageLink = i.Comics.comicImageLink;
+                delete i["Comics"];
+            });
+            setData(comic);
+        }
+        getData(profile)
+    },[comicIdToDelete])
     
-    const data = await getData(profile);
-    
-
     if (data.length == 0) return(<h1>không tìm thấy truyện!</h1>);
-    data.forEach((i: any) => {
-        i.id = i.Comics.id;
-        i.comicName = i.Comics.comicName;
-        i.comicImageLink = i.Comics.comicImageLink;
-        delete i["Comics"];
-    });
+    
     return (
     <div>
-        <Container data={data}/>
+        {data.length > 0 && <Container setcomicIdToDelete={setcomicIdToDelete} data={data}/>}
     </div>
   )
 }
