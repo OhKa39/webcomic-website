4529ca19044a0ddeedbcca241ec74289b1c6cc5d
Naalaya <117615727+Naalaya@users.noreply.github.com>
2024-09-26 10:45:00 +0700

    Next Config: upload crawl address

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'i.truyenvua.com',
-            pathname: '**',
-            
-        },
-        {
-            protocol: 'https',
-            hostname: 'comicbucket23.s3.ap-southeast-1.amazonaws.com',
-            pathname: '**',
-            
-        },
-        {
-            protocol: 'https',
-            hostname: 'img.clerk.com',
-            pathname: '**',
-        }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.truyenvua.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "comicbucket23.s3.ap-southeast-1.amazonaws.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.hinhhinh.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/a/dd8jxjsqkw/*",
+      },
     ],
-        unoptimized: true
-    },
+    unoptimized: true,
+  },
 };
-
+//Accept I.hinhhinh.com
 export default nextConfig;
